7b12e3d74928c0c579692b666c905491df42d6fb
west-field <114457451+west-field@users.noreply.github.com>
2023-05-30 15:26:35 +0900

    繰り返し違う場所で使う情報

--- Util/Info.h
+++ Util/Info.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "Geometry.h"
+
 //キャラクターの種類
 enum class CharType
 {
---
+++ Util/menuInfo.h
@@ -0,0 +1,10 @@
+#pragma once
+
+struct Element
+{
+	int x, y;//À•W
+	int color;//F
+	const wchar_t* name;//–¼‘O
+	int nameNum;
+	int fontSize;
+};
